136431af1df3d219bbe47d66c171b84dda3db5e5
Lucy <lucy@nix2twink.gay>
2024-11-07 12:47:43 +0100

    foo

--- init.el
+++ init.el
@@ -15,9 +15,11 @@
 ;;; UNDO
 ;; Vim style undo not needed for emacs 28
 (use-package undo-fu)
-(use-package gruvbox-theme
- :config
- (load-theme 'gruvbox t))
+
+;; default theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/tokyonight")
+(load-theme 'tokyo t)
+
 ;;; Vim Bindings
 (use-package evil
   :demand t
@@ -111,3 +113,45 @@
 
 
 (add-hook python-mode-hook 'lsp-deferred)
+
+;; javascript
+
+(use-package typescript-mode
+  :after tree-sitter
+  :config
+  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+;; https://github.com/orzechowskid/tsi.el/
+;; great tree-sitter-based indentation for typescript/tsx, css, json
+(use-package tsi
+  :after tree-sitter
+  :quelpa (tsi :fetcher github :repo "orzechowskid/tsi.el")
+  ;; define autoload definitions which when actually invoked will cause package to be loaded
+  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+  :init
+  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
+  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
+  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
+  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
+
+
+;; auto-format different source code files extremely intelligently
+;; https://github.com/radian-software/apheleia
+(use-package apheleia
+  :ensure t
+  :config
+  (apheleia-global-mode +1))
+
+
+(use-package eglot
+  :ensure t)
+;; javascript
